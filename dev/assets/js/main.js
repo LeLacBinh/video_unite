a32aaac7bf18e8f8c7e25c08d9bea2a7ba19a573
AGL_Lê Lạc Bình (ビン) <le_lac_binh@allgrow-labo.jp>
2022-05-04 13:44:06 +0700

    Finish .c-pick-up

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
+    $('.c-pick-up__slide').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        arrows: true,
+        dots: true,
+        variableWidth: true,
+        focusOnSelect: true,
+        centerPadding: '55px',
+        prevArrow: $('.c-pick-up .prev'),
+        nextArrow: $('.c-pick-up .next'),
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerPadding: '20px',
+                variableWidth: false
+              }
+            }
+        ]
+    });
+    // --- AOS ---
     AOS.init();
 })
