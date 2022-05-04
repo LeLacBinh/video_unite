1a03334da9743c49d7c4e9367d2f3104928870ee
AGL_Nguyễn Cao Quốc Hùng(フン) <nguyen_cao_quoc_hung@allgrow-labo.jp>
2022-05-04 16:27:07 +0700

    create: plan, flow

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
     AOS.init();
+
+    // Header Button Nav
+    $(function() {
+        $('.c-trigger').on('click', function() {
+            $(this).toggleClass('is-active');
+            $('.c-header').toggleClass('is-active');
+            return false;
+        });
+    });
+
+    //Add padding top
+    $("main").css({
+        "padding-top": $(".c-header").outerHeight()
+    });
 })
